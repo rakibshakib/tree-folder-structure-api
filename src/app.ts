0359d31f03a5140db185748b823e3b5043ffcc07
iboscopilot <eaccount@ibos.io>
2023-10-18 14:27:56 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/", router);
+
+//global error handler
+// app.use(globalErrorHandler);
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    success: true,
+    message: "Welcome to Folder Structure API",
+  });
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: "Not Found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "API Not Found",
+      },
+    ],
+  });
+  next();
+});
+
+export default app;
---
+++ src/db.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function serverStart() {
+  try {
+    await mongoose.connect(
+      "mongodb://localhost:27017/learningPortal" as string
+    );
+    console.log("Database Connected....😃");
+    app.listen(config.port, () => {
+      console.log(`Server listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log("Failed To Connect Database..😥", error);
+  }
+}
+serverStart();
